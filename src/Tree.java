4b2c0c360cf964a5fdcca1e7975571d4c93a807c
kianchen <bluekian10@gmail.com>
2022-09-15 10:35:32 -0700

    finished and tested tree

--- src/Tree.java
+++ src/Tree.java
@@ -9,13 +9,15 @@ public class Tree {
 	String content;
 	
 	public Tree (ArrayList<String> keyVal) throws FileNotFoundException {
+		content="";
 		for (String obj: keyVal) {
 			content=content+obj+"\n";
 		}
 		content = content.substring(0, content.length()-1);
 		String sha1 = encrypt(content);
-		PrintWriter writer = new PrintWriter (new File ("objects/" + sha1));
+		PrintWriter writer = new PrintWriter (new File ("Objects/" + sha1));
 		writer.print(content);
+		writer.close();
 	}
 
 	private String encrypt(String fileContent) {
@@ -29,5 +31,13 @@ public class Tree {
 			e.printStackTrace();
 		}
 		return sha1;
+	}
+	
+	public static void main (String [] args) throws FileNotFoundException {
+		ArrayList<String> test = new ArrayList<String>();
+		test.add("blob : c09f382894b42abb22deaef2b26ca5b008334cf7");
+		test.add("tree : 60eaad68490578f099fc5f29fbab9029561198e5");
+		test.add("blob : db2c0fa24afb6334ce69488262c5ba671312207a");
+		Tree testTree = new Tree(test);
 	}
 }
