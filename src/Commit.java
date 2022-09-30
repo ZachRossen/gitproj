15e958a191d670804903049014f309e2bb41a35c
Zach Rossen <zachary.rossen@gmail.com>
2022-09-30 10:39:34 -0700

    Git and commit test

--- src/Commit.java
+++ src/Commit.java
@@ -14,16 +14,17 @@ import java.util.ArrayList;
 public class Commit {
 	private String parent;
 	private String other;
-	private String pTree;
+	private Tree pTree;
 	private String author;
 	private String date;
 	private String summary;
-	public Commit(String tree,String summ,String auth,String prent){
-		pTree = tree;
+
+	public Commit(String summ, String auth, String prent) {
 		parent = prent;
 		summary = summ;
 		author = auth;
 	}
+
 	public String genSha(File f) throws IOException, NoSuchAlgorithmException {
 		BufferedReader r = new BufferedReader(new FileReader(f));
 		ArrayList<String> strs = new ArrayList<String>();
@@ -32,67 +33,69 @@ public class Commit {
 
 		}
 		r.close();
-		String s ="";
-		for (int i =0 ; i<4;i++) {
-			if (strs.size()>i) {
-				s+=strs.get(strs.size()-i);
+		String s = "";
+		for (int i = 0; i < 4; i++) {
+			if (strs.size() > i) {
+				s += strs.get(strs.size() - i);
 			}
-			
+
 		}
-		date = strs.get(strs.size()-1);
+		date = strs.get(strs.size() - 1);
 		MessageDigest md = MessageDigest.getInstance("SHA-1");
-		 
-        // digest() method is called
-        // to calculate message digest of the input string
-        // returned as array of byte
-        byte[] messageDigest = md.digest(s.getBytes());
 
-        // Convert byte array into signum representation
-        BigInteger no = new BigInteger(1, messageDigest);
+		// digest() method is called
+		// to calculate message digest of the input string
+		// returned as array of byte
+		byte[] messageDigest = md.digest(s.getBytes());
 
-        // Convert message digest into hex value 
-        String hashtext = no.toString(16);
-        return hashtext;
+		// Convert byte array into signum representation
+		BigInteger no = new BigInteger(1, messageDigest);
+
+		// Convert message digest into hex value
+		String hashtext = no.toString(16);
+		return hashtext;
 	}
+
 	public String getDate() {
-		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
-		LocalDateTime now = LocalDateTime.now();  
-		date =(dtf.format(now));  
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		date = (dtf.format(now));
 		return date;
-	   }
+	}
+
 	public void writeNew() throws NoSuchAlgorithmException, IOException {
-		String s = pTree;
-		s+="\n";
-		s+=parent;
-		s+="\n";
-		s+=other;
-		s+="\n";
-		s+=author;
-		s+="\n";
-		s+=date;
-		s+="\n";
-		s+=summary;
+		String s = pTree.;
+		s += "\n";
+		s += parent;
+		s += "\n";
+		s += other;
+		s += "\n";
+		s += author;
+		s += "\n";
+		s += date;
+		s += "\n";
+		s += summary;
 		MessageDigest md = MessageDigest.getInstance("SHA-1");
-		 
-        // digest() method is called
-        // to calculate message digest of the input string
-        // returned as array of byte
-        byte[] messageDigest = md.digest(s.getBytes());
 
-        // Convert byte array into signum representation
-        BigInteger no = new BigInteger(1, messageDigest);
+		// digest() method is called
+		// to calculate message digest of the input string
+		// returned as array of byte
+		byte[] messageDigest = md.digest(s.getBytes());
 
-        // Convert message digest into hex value 
-        String hashtext = no.toString(16);
-        File f = new File("objects\\"+hashtext);
-        BufferedWriter wr = new BufferedWriter(new FileWriter(f));
+		// Convert byte array into signum representation
+		BigInteger no = new BigInteger(1, messageDigest);
+
+		// Convert message digest into hex value
+		String hashtext = no.toString(16);
+		File f = new File("objects\\" + hashtext);
+		BufferedWriter wr = new BufferedWriter(new FileWriter(f));
 		wr.write(s);
 		wr.close();
-        f.createNewFile();
+		f.createNewFile();
 	}
-	
-	public static void main(String [] args) throws NoSuchAlgorithmException, IOException {
-		Commit hi = new Commit("tree","sum","auth","prent");
+
+	public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+		Commit hi = new Commit("sum", "auth", "prent");
 		hi.getDate();
 		hi.writeNew();
 	}
---
+++ src/Git.java
@@ -0,0 +1,21 @@
+
+public class Git {
+
+	// index
+	private Index index;
+
+	// constructor
+	public Git() {
+
+	}
+
+	// initializes index and objects folder
+	public void init() throws Exception {
+		index = new Index();
+	}
+
+	// creates blob and adds to index
+	public void add(String fileName) throws Exception {
+		index.addBlob(fileName);
+	}
+}
